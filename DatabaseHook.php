66c97062a7b7b51cf14bcde5c8611409af35cde2
Warren <warren@onehomebase.com>
2015-05-11 22:30:05 +0800

    Create a generic DatabaseHook class which implements ParserHookInterface that is not part of the package.

---
+++ DatabaseHook.php
@@ -0,0 +1,13 @@
+<?php
+
+use WarrenCA\PhilippineTowns\ParserHookInterface;
+
+class DatabaseHook implements ParserHookInterface {
+	public function save(Array $params)
+	{
+		// connect to database
+		// catch the parameters
+		// create your logic
+		// print_r($params);
+	}
+}
